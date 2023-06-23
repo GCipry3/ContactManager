7c542398d7c328a663c042d85a48886fcbf1afbc
Galbeaza Ciprian (uie82714) <ciprian.galbeaza@continental-corporation.com>
2023-06-23 17:48:41 +0300

    Replace the module with the necessary functions from the module

--- http-example.js
+++ http-example.js
@@ -1,13 +1,23 @@
-const http = require('http')
+const {request, get} = require('http')
 
-const req = http.request('http://www.google.com', (res)=>{
+const req = request('http://www.google.com', (res)=>{
     res.on('data',(chunk)=>{
         console.log(`Data chunk is ${chunk}`)
     })
 
     res.on('end',()=>{
-        console.log('!!No more data!!')
+        console.log('!!No more data1!!')
     })
 })
 
 req.end()
+
+get('http://www.google.com', (res)=>{
+    res.on('data',(chunk)=>{
+        console.log(`Data chunk is ${chunk}`)
+    })
+
+    res.on('end',()=>{
+        console.log('!!No more data2!!')
+    })
+})
