a8d11aa3b834db3857c69bfc8741e20919ecad1a
Galbeaza Ciprian (uie82714) <ciprian.galbeaza@continental-corporation.com>
2023-06-23 17:10:01 +0300

    Test observer design pattern

--- hello.js
+++ hello.js
@@ -1,3 +1,11 @@
-console.log('Hi')
-const variable = 'var'
-console.log(`variable ${variable}`)
+const EventEmitter = require('events');
+const celebrity = new EventEmitter();
+
+celebrity.on('race win', function(){
+    console.log('Congratulations, you are the best!');
+})
+celebrity.on('race win', function(){
+    console.log('Booo!');
+})
+
+celebrity.emit('race win')
