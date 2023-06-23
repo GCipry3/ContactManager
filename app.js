91cf745006bf062dfc036e5dbfd9fcfa1d44ed5e
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-23 22:21:31 +0200

    Add nodemon dependencies and create a basic server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+// Set an env var for the PORT, and a default value
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+
+app.listen(port,()=>{
+    console.log(`App listening on port ${port}`)
+})
