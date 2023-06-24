e18b3e02d129649612c150ba3e09c3efa16e8041
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-25 00:34:03 +0200

    Correct the errors from the following routes: POST /users and GET /

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ const app = express()
 // Set an env var for the PORT, and a default value
 const port = process.env.PORT || 3000;
 
-const connection = postgreSQL.Client({
+const client = new postgreSQL.Client({
     connectionString: process.env.DATABASE_URL
 })
 
-app.get('/',(req,res)=>{
-    res.send('Hello World! hihihi')
-})
+app.use(express.json())
+
+client.connect();
+
+app.get('/', (req, res) => {
+  client.query('SELECT * FROM users', (err, dbRes) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Database query failed' });
+    } else {
+      res.json(dbRes.rows);
+    }
+  });
+  console.log('Hello World')
+  // res.send('Hello!!!')
+});
 
 app.post('/users', async (req, res) => {
     const { username, password } = req.body;
     try {
-        await connection.connect();
-        const result = await connection.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+        const result = await client.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
         res.status(201).json({ status: 'success', message: 'user created' });
     } catch (err) {
         console.error(err);
         res.status(500).json({ status: 'error', message: 'internal server error' });
-    } finally {
-        await connection.end();
     }
 });
 
