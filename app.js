d02da7fc84b10013e96bbce22e3f41d3fe334b4b
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-24 23:56:27 +0200

    Create a postresql db connection (to AWS RDS) and the route for inserting a user

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+const postgreSQL = require('pg')
 const express = require('express')
 const app = express()
 // Set an env var for the PORT, and a default value
 const port = process.env.PORT || 3000;
 
+const connection = postgreSQL.Client({
+    connectionString: process.env.DATABASE_URL
+})
+
 app.get('/',(req,res)=>{
     res.send('Hello World! hihihi')
 })
+
+app.post('/users', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        await client.connect();
+        const result = await client.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, password]);
+        res.status(201).json({ status: 'success', message: 'user created' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ status: 'error', message: 'internal server error' });
+    } finally {
+        await client.end();
+    }
+});
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`)
