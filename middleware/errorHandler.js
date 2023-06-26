3027905fd80cdff82e23421c2e48962133c6eb74
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-26 17:01:32 +0200

    Create a contact schema for our contacts and finish the REST API routes

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -9,26 +9,21 @@ const constants = {
 const errorHandler = (err,req,res,next)=>{
     const statusCode = res.statusCode ? res.statusCode : 500
     switch(statusCode){
-        case VALIDATION_ERROR:
+        case constants.VALIDATION_ERROR:
             res.json({title:"Validation Error",message:err.message, stackTrace: err.stack})
             break;
-        case UNAUTHORIZED:
+        case constants.UNAUTHORIZED:
             res.json({title:"Unauthorized",message:err.message, stackTrace: err.stack})
             break;
-        case FORBIDDEN:
+        case constants.FORBIDDEN:
             res.json({title:"Forbidden",message:err.message, stackTrace: err.stack})
             break;
-        case NOT_FOUND:
+        case constants.NOT_FOUND:
             res.json({title:"Validation Error",message:err.message, stackTrace: err.stack})
             break;
-        case SERVER_ERROR:
+        case constants.SERVER_ERROR:
             res.json({title:"Server Error",message:err.message, stackTrace: err.stack})
             break;
-        
-        default:
-            sonsole.log("No Error!")
-            break;
-
     }
 }
 
