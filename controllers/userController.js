dfdf85fa09cd8cf62507a0d9dff1afcb84c49724
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-26 17:56:12 +0200

    Implement the register functionality and the password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,35 @@
 const asyncHandler = require("express-async-handler");
+const User = require('../models/userModel')
+const bcrypt = require("bcrypt")
 
 const registerUser = asyncHandler (async (req,res) => {
+    const {username, email, password } = req.body;
+    if(!username || !email || !password){
+        res.statusCode = 400
+        throw new Error("All fields are mandatory")
+    }
+    const userAvailable = await User.findOne({email})
+    if(userAvailable){
+        res.status(400)
+        throw new Error("User already registered")
+    }
+
+    //Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(200).json({_id:user.id,email:user.email})
+    }else{
+        res.status(400)
+        throw new Error("User data is not valid")
+    }
+
     res.json({message : "Register the user"})
 })
 
