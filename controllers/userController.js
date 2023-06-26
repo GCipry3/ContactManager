e5efcd012e3119ccae7f5c0f48c686f11c6d0077
GCipry3 <galbeazaciprian30@gmail.com>
2023-06-26 19:40:30 +0200

    Implement the authentication method for the current user using jsonwebtoken

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const User = require('../models/userModel')
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const registerUser = asyncHandler (async (req,res) => {
     const {username, email, password } = req.body;
     if(!username || !email || !password){
-        res.statusCode = 400
+        res.status(400)
         throw new Error("All fields are mandatory")
     }
     const userAvailable = await User.findOne({email})
@@ -34,11 +35,32 @@ const registerUser = asyncHandler (async (req,res) => {
 })
 
 const loginUser = asyncHandler (async (req,res) => {
-    res.json({message : "Login the user"})
+    const {email,password} = req.body
+    if (!email || !password){
+        res.status(400)
+        throw new Error("All fields are mandatory")
+    }
+
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password,user.password))){
+        const accessToken = jwt.sign({
+            user:{
+                username: user.username,
+                email: user.email,
+                id: user.id
+            }
+            }, process.env.ACCESS_TOKEN_SECRET,
+            {expiresIn: "1m"}
+        )
+        res.status(200).json({accessToken})
+    }else{
+        res.status(401)
+        throw new Error("Email or password is not valid")
+    }
 })
 
 const currentUser = asyncHandler (async (req,res) => {
-    res.json({message : "Current user info"})
+    res.json(req.user)
 })
 
 module.exports = {registerUser,loginUser,currentUser}
